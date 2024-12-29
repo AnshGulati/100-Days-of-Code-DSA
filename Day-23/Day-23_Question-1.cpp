dc3a93133a01160e6ce48690f2fcaa9ce728ce4d
Ansh Gulati <124679547+AnshGulati@users.noreply.github.com>
2024-12-30 00:11:33 +0530

    Updated Day-23

--- Day-23/Day-23_Question-1.cpp
+++ Day-23/Day-23_Question-1.cpp
@@ -1,13 +1,5 @@
-// 1903. Largest Odd Number in a String
-
-// You are given a string num, representing a large integer.
-// Return the largest-valued odd integer (as a string) that is a non-empty substring of num,
-// or an empty string "" if no odd integer exists.
-// A substring is a contiguous sequence of characters within a string.
-
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 class Solution
@@ -19,7 +11,7 @@ public:
         string result;
         for (int i = num.length() - 1; i >= 0; i--)
         {
-            if (num[i] % 2 == 1)
+            if ((num[i] - '0') % 2 == 1) // Check if the character represents an odd digit
             {
                 result = num.substr(0, i + 1);
                 oddFound = true;
@@ -31,3 +23,20 @@ public:
         return result;
     }
 };
+
+int main()
+{
+    string num;
+    cout << "Enter a large integer: ";
+    cin >> num;
+
+    Solution solution;
+    string largestOdd = solution.largestOddNumber(num);
+
+    if (largestOdd.empty())
+        cout << "No odd number exists in the input string." << endl;
+    else
+        cout << "The largest odd number in the string is: " << largestOdd << endl;
+
+    return 0;
+}
